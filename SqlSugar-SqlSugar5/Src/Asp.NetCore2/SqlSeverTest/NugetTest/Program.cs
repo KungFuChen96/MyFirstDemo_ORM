839042c033eef89b58d6250f10b1658d7943a0e3
chenguangfan <chenguangfan@neware.com.cn>
2020-08-07 14:25:54 +0800

    add New Project

---
+++ SqlSugar-SqlSugar5/Src/Asp.NetCore2/SqlSeverTest/NugetTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SqlSugar;
+namespace NugetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new SqlSugarClient(new ConnectionConfig() {
+                 ConnectionString=Config.ConnectionString,
+                 IsAutoCloseConnection=true,
+                 DbType=DbType.SqlServer
+            });
+            var list = db.Ado.GetInt("select 1");
+            Console.WriteLine("Hello World!");
+        }
+        public class Config
+        {
+            public static string ConnectionString = "server=.;uid=sa;pwd=haosql;database=SQLSUGAR4XTEST";
+        }
+    }
+}
